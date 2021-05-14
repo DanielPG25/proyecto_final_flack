45ce05da880a997a49c0e0edae65283077b2ce49
Daniel Parrales García <dannyparrales16@gmail.com>
2021-05-14 12:10:17 +0200

    terminado el programa para flitrar por nombre de artículo y empezado el programa para mostrar los detalles de un artículo en concreto

---
+++ gamespot3.py
@@ -0,0 +1,15 @@
+import requests
+import os
+import sys
+
+url_base= "https://www.gamespot.com/api/"
+key=os.environ["KEY"]
+nombre="title:" + input("Dime el nombre del artículo: ")
+parametros={"api_key":key,"format":"json","filter":nombre,"offset":0}
+cabeceras={"User-Agent":"Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0"}
+r=requests.get(url_base+"articles/",params=parametros,headers=cabeceras)
+print(r.url)
+if r.status_code==200:
+	doc = r.json()
+	for juegos in doc.get('results'):
+		print(juegos.get('title'),' ',juegos.get('id'))
